6824b16afb544aa21f976e161a948decd1fb8570
ythokt <araashibutani@gmail.com>
2017-10-22 14:34:39 +0800

    test01

---
+++ js/all.js
@@ -0,0 +1,12 @@
+//這是標題的事件
+$(document).ready(function(){
+	$('.button1').click(function(event){
+		$('.text').fadeToggle(1400);
+	});
+	$('h3').click(function(event){
+		$('.img').toggleClass('active');
+	});
+	$('.button2').click(function(event) {
+		$('.box1').slideUp(1000).slideDown(1000).fadeIn(1000).fadeOut(1000).slideUp(1000).slideDown(1000).fadeIn(1000);
+	});
+}); 
